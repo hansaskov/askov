ece8b2b3b4650824acb3cb54cbf2bdffb5bc6c15
hansaskov <hansaskov1999@gmail.com>
2023-10-30 23:32:37 +0100

    Update responive sizeing

--- src/lib/MisoSection.svelte
+++ src/lib/MisoSection.svelte
@@ -4,16 +4,14 @@
 		{ path: 'images/miso_1.jpg', description: 'Miso slapper af og kigger på fugle' },
 		{
 			path: 'images/miso_2.jpg',
-			description:
-				'Miso ligger på gulvet og kigger på kameraet med et dybt og blodtørstigt blik. "Hvis bare jeg var stor nok til at spise den lækre mand"'
+			description: 'Miso ligger på gulvet og kigger på kameraet'
 		},
 		{ path: 'images/miso_4.jpg', description: 'Miso ligger på sofaen' }
 	];
 </script>
 
 <div class="bg-background text-text py-10" {...$$restProps}>
-	<h1 class="text-5xl font-bold text-primary mb-8 text-center">Mød Miso, Vores ynglings kat</h1>
-	<p class="text-lg text-center mb-16">Indtil videre ;)</p>
+	<h1 class=" text-3xl lg:text-5xl font-bold text-primary mb-8 text-center">Mød Miso, Vores ynglings kat!</h1>
 
 	{#each images as image, index}
 		<div class={`flex ${index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'} items-center my-4`}>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,15 @@
 <!-- src/routes/+layout.svelte or src/routes/+page.svelte -->
 <script>
-    import HeroSection from '$lib/HeroSection.svelte';
-    import MisoSection from '$lib/MisoSection.svelte';
+	import HeroSection from '$lib/HeroSection.svelte';
+	import MisoSection from '$lib/MisoSection.svelte';
 </script>
 
 <div class="h-screen overflow-y-scroll snap-y snap-proximity pt-4">
-    <div class="snap-start h-screen snap-always">
-        <HeroSection />
-    </div>
-    <div class="snap-start min-h-screen snap-always">
-        <MisoSection />
-        <!-- Additional content here if needed to allow scrolling within this section -->
-    </div>
+	<div class="snap-start h-screen snap-always">
+		<HeroSection />
+	</div>
+	<div class="snap-start min-h-screen snap-always">
+		<MisoSection />
+		<!-- Additional content here if needed to allow scrolling within this section -->
+	</div>
 </div>
