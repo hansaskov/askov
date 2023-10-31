f7b1ed243a3b9e37dc43514fd6bc7187477d2291
hansaskov <hansaskov1999@gmail.com>
2023-10-31 18:49:28 +0100

    Update miso types and remove unused dependencies

--- src/lib/MisoSection.svelte
+++ src/lib/MisoSection.svelte
@@ -1,28 +1,28 @@
 <script>
+	/** @type {{ path: string, description: string }[]} */
 	const images = [
 		{ path: 'images/miso_3.jpg', description: 'Miso solbader i vores lænestol' },
 		{ path: 'images/miso_7.jpg', description: 'Miso har været hos lægen' },
-		{
-			path: 'images/miso_8.jpg',
-			description: 'Miso har spillet Mario Kart med familien'
-		},
-		{ path: 'images/miso_1.jpg', description: 'Miso ligger udenfor og ser på fulge'},
+		{ path: 'images/miso_8.jpg', description: 'Miso har spillet Mario Kart med familien' },
+		{ path: 'images/miso_1.jpg', description: 'Miso ligger udenfor og ser på fulge' },
 		{ path: 'images/miso_5.JPG', description: 'Miso sover udenfor' }
 	];
 </script>
 
 <div class="bg-background text-text py-10" {...$$restProps}>
-	<h1 class=" text-3xl lg:text-5xl font-bold text-primary mb-8 text-center">Mød Miso, Vores ynglings kat!</h1>
+	<h1 class=" text-3xl lg:text-5xl font-bold text-primary mb-8 text-center">
+		Mød Miso, Vores ynglings kat!
+	</h1>
 
-	{#each images as image, index}
+	{#each images as { path, description }, index}
 		<div class={`flex ${index % 2 === 0 ? 'flex-row' : 'flex-row-reverse'} items-center my-4`}>
 			<img
-				src={image.path}
+				src={path}
 				alt="Miso"
 				class={`w-[49%] shadow-lg ${index % 2 === 0 ? 'rounded-r-xl' : 'rounded-l-xl'}`}
 			/>
 			<p class="flex-1 text-md sm:text-xl lg:text-3xl mx-8 text-center text-primary p-4 rounded-lg">
-				{image.description}
+				{description}
 			</p>
 		</div>
 	{/each}
