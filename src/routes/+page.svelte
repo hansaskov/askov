d4a16cd7220c7ba160eedb86d091fc4fa48113db
hansaskov <hansaskov1999@gmail.com>
2023-11-14 00:30:28 +0100

    Added theme picker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,57 @@
 	<div class="snap-start h-screen snap-always">
 		<HeroSection />
 	</div>
+
+	<div
+		class="flex justify-between navbar bg-base-100 bg-opacity-90 shadow-lg rounded-box sticky top-0 mx-4 w-auto max-w-full"
+	>
+		<div>
+			<a href="/" class="btn btn-ghost text-2xl">Askov</a>
+		</div>
+		<div>
+			<div class="dropdown">
+				<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+				<!-- svelte-ignore a11y-label-has-associated-control -->
+				<label tabindex="0" class="btn text-2xl btn-ghost m-1">
+					Tema
+					<svg
+						width="12px"
+						height="12px"
+						class="h-2 w-2 fill-current opacity-60 inline-block"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 2048 2048"
+						><path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z" /></svg
+					>
+				</label>
+				<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+				<ul
+					tabindex="0"
+					class="dropdown-content z-[1] p-2 shadow-2xl bg-base-200 bg-opacity-90 rounded-box w-52"
+				>
+					<li>
+						<input
+							type="radio"
+							name="theme-dropdown"
+							class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+							aria-label="Forår"
+							value="default"
+						/>
+					</li>
+					<li>
+						<input
+							type="radio"
+							name="theme-dropdown"
+							class="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+							aria-label="Efterår"
+							value="october"
+						/>
+					</li>
+				</ul>
+			</div>
+			<a href="onskeliste" class="btn btn-ghost text-2xl">Ønskeliste</a>
+		</div>
+	</div>
+
 	<div class="snap-start min-h-screen snap-always">
 		<MisoSection />
 	</div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-	plugins: [require("@tailwindcss/typography"), require("daisyui")],
+	plugins: [require('@tailwindcss/typography'), require('daisyui')],
 	daisyui: {
 		themes: [
 			{
-			  mytheme: {
-				"primary": "rgb(113, 142, 107)",
-				"secondary": "rgb(223, 220, 229)",
-				"accent": "rgb(133, 105, 140)",
-				"neutral": "rgb(27, 29, 22)",
-				"base-100": "rgb(239, 240, 235)",
-			  },
-			},
-		],
+				garden: {
+					primary: 'rgb(113, 142, 107)',
+					secondary: 'rgb(223, 220, 229)',
+					accent: 'rgb(133, 105, 140)',
+					neutral: 'rgb(27, 29, 22)',
+					'base-100': 'rgb(239, 240, 235)'
+				},
+				october: {
+					primary: '#ff6d4d',
+					secondary: '#e6e6e5',
+					accent: '#d12600',
+					neutral: '#262626',
+					'base-100': '#fafafa'
+				}
+			}
+		]
 	}
 };
