532c1fa15cbf12f5fd7a0e268e6f1e1b507578e0
hansaskov <hansaskov1999@gmail.com>
2023-11-14 10:14:11 +0100

    Update sizeing

--- src/lib/Header.svelte
+++
@@ -1,7 +0,0 @@
-<!-- src/lib/Header.svelte -->
-<header class="bg-primary p-4 shadow-xl">
-	<div class="container mx-auto flex justify-between items-center">
-		<h1 class="text-3xl font-bold">Askov</h1>
-		<!-- Additional navigation or elements can be added here -->
-	</div>
-</header>
