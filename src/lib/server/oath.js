83e557e1c0e166232bcd99835a148b20dc410d72
hansaskov <hansaskov1999@gmail.com>
2023-11-18 15:25:56 +0100

    Update oath redirects

--- src/lib/server/oath.js
+++ src/lib/server/oath.js
@@ -1,10 +1,10 @@
 import { google } from '@lucia-auth/oauth/providers';
 import { auth } from '$lib/server/lucia';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
-import { base } from '$app/paths';
+import { dev } from '$app/environment';
 
 export const googleAuth = google(auth, {
-	redirectUri: base,
+	redirectUri: dev ? 'http://localhost:5173/login/google/callback' : 'https://askov.dk/login/google/callback',
 	clientId: GOOGLE_CLIENT_ID,
 	clientSecret: GOOGLE_CLIENT_SECRET
 });
