8fbd6c87424c1180e898edfcfc36f6a900930622
hansaskov <hansaskov1999@gmail.com>
2023-11-18 15:12:54 +0100

    Update path redirect

--- src/lib/server/oath.js
+++ src/lib/server/oath.js
@@ -1,9 +1,10 @@
 import { google } from '@lucia-auth/oauth/providers';
 import { auth } from '$lib/server/lucia';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
+import { base } from '$app/paths';
 
 export const googleAuth = google(auth, {
-	redirectUri: '/',
+	redirectUri: base,
 	clientId: GOOGLE_CLIENT_ID,
 	clientSecret: GOOGLE_CLIENT_SECRET
 });
